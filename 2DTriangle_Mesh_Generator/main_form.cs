aa8f9f06e443e0110cf234370e8a9816c3787567
Mano Sundar <saminnx@gmail.com>
2022-08-27 23:40:03 -0300

    Add project files.

---
+++ 2DTriangle_Mesh_Generator/main_form.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2DTriangle_Mesh_Generator
+{
+    public partial class main_form : Form
+    {
+        public main_form()
+        {
+            InitializeComponent();
+        }
+    }
+}
